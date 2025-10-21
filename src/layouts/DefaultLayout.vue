e5e0ba8f4ac0fc76a088bb4a1fb7b852399115dc
omatudev <developer@omatu.dev>
2025-10-21 04:37:05 -0600

    feat: add Vercel analytics and speed insights to DefaultLayout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
   <main class="grid h-full w-full gap-4 p-4">
+    <Analytics />
+    <SpeedInsights />
     <RouterView />
   </main>
 </template>
